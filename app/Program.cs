3d1a9407aed11314802775a2a1697c78311f16d6
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-11-24 18:18:13 -0300

    removed dotnet app

--- app/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Microsoft.Data.SqlClient;
-
-Console.WriteLine("Hello, World!");
-
-string connectionString = "Data Source=server63; Initial Catalog=Clinic; Integrated Security=true; Column Encryption Setting=enabled";
-SqlConnection connection = new(connectionString);
-
-
